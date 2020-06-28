a79443d66e043ffb6a54d506763089fb23b4947a
hfdxiaoyu <1306933520@qq.com>
2020-06-28 10:00:59 +0800

    提交更新

--- demo1/src/main/java/com/itheima/service/impl/PersonServiceImpl.java
+++ demo1/src/main/java/com/itheima/service/impl/PersonServiceImpl.java
@@ -1,4 +1,4 @@
-package com.itheima.service;
+package com.itheima.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.itheima.dao.PersonDao;
 import com.itheima.po.Person;
+import com.itheima.service.PersonService;
 @Service
 @Transactional
 public class PersonServiceImpl implements PersonService {
